573b8cf69c0bc5f4989af5228c967be8c036f354
MrGS <111638924+Gswaraw37@users.noreply.github.com>
2023-10-05 21:18:55 +0700

    Add files via upload

---
+++ symalas_ani.sql
@@ -0,0 +1,408 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Okt 2023 pada 16.13
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `symalas_ani`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dosens`
+--
+
+CREATE TABLE `dosens` (
+  `nip` varchar(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwals`
+--
+
+CREATE TABLE `jadwals` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `hari` enum('senin','selasa','rabu','kamis','jumat','sabtu','minggu') NOT NULL,
+  `waktu` varchar(10) NOT NULL,
+  `ruang` varchar(5) NOT NULL,
+  `kd_matkul` varchar(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswas`
+--
+
+CREATE TABLE `mahasiswas` (
+  `npm` varchar(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `sosmed` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`sosmed`)),
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `matkuls`
+--
+
+CREATE TABLE `matkuls` (
+  `kd_matkul` varchar(20) NOT NULL,
+  `nama_matkul` varchar(50) NOT NULL,
+  `nip` varchar(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(2, '2019_08_19_000000_create_failed_jobs_table', 1),
+(3, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(4, '2023_09_15_143020_create_dosens_table', 1),
+(5, '2023_09_16_143110_create_mahasiswas_table', 1),
+(6, '2023_09_17_000000_create_users_table', 1),
+(7, '2023_09_18_143008_create_matkuls_table', 1),
+(8, '2023_10_04_142940_create_jadwals_table', 1),
+(9, '2023_10_04_142957_create_tugas_table', 1),
+(10, '2023_10_04_143028_create_penggantis_table', 1),
+(11, '2023_10_04_143057_create_verify_tugas_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penggantis`
+--
+
+CREATE TABLE `penggantis` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `hari` enum('senin','selasa','rabu','kamis','jumat','sabtu','minggu') NOT NULL,
+  `waktu` varchar(10) NOT NULL,
+  `ruang` varchar(5) NOT NULL,
+  `kd_matkul` varchar(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tugas`
+--
+
+CREATE TABLE `tugas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `kd_matkul` varchar(20) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `tautan` text NOT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','member','dosen') NOT NULL DEFAULT 'member',
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `verify_tugas`
+--
+
+CREATE TABLE `verify_tugas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `npm` varchar(20) NOT NULL,
+  `kd_matkul` varchar(20) NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `dosens`
+--
+ALTER TABLE `dosens`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indeks untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indeks untuk tabel `jadwals`
+--
+ALTER TABLE `jadwals`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jadwals_kd_matkul_foreign` (`kd_matkul`);
+
+--
+-- Indeks untuk tabel `mahasiswas`
+--
+ALTER TABLE `mahasiswas`
+  ADD PRIMARY KEY (`npm`),
+  ADD UNIQUE KEY `mahasiswas_email_unique` (`email`);
+
+--
+-- Indeks untuk tabel `matkuls`
+--
+ALTER TABLE `matkuls`
+  ADD PRIMARY KEY (`kd_matkul`),
+  ADD KEY `matkuls_nip_foreign` (`nip`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indeks untuk tabel `penggantis`
+--
+ALTER TABLE `penggantis`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `penggantis_kd_matkul_foreign` (`kd_matkul`);
+
+--
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indeks untuk tabel `tugas`
+--
+ALTER TABLE `tugas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tugas_kd_matkul_foreign` (`kd_matkul`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indeks untuk tabel `verify_tugas`
+--
+ALTER TABLE `verify_tugas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `verify_tugas_npm_foreign` (`npm`),
+  ADD KEY `verify_tugas_kd_matkul_foreign` (`kd_matkul`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `jadwals`
+--
+ALTER TABLE `jadwals`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `penggantis`
+--
+ALTER TABLE `penggantis`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tugas`
+--
+ALTER TABLE `tugas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `verify_tugas`
+--
+ALTER TABLE `verify_tugas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `jadwals`
+--
+ALTER TABLE `jadwals`
+  ADD CONSTRAINT `jadwals_kd_matkul_foreign` FOREIGN KEY (`kd_matkul`) REFERENCES `matkuls` (`kd_matkul`);
+
+--
+-- Ketidakleluasaan untuk tabel `matkuls`
+--
+ALTER TABLE `matkuls`
+  ADD CONSTRAINT `matkuls_nip_foreign` FOREIGN KEY (`nip`) REFERENCES `dosens` (`nip`);
+
+--
+-- Ketidakleluasaan untuk tabel `penggantis`
+--
+ALTER TABLE `penggantis`
+  ADD CONSTRAINT `penggantis_kd_matkul_foreign` FOREIGN KEY (`kd_matkul`) REFERENCES `matkuls` (`kd_matkul`);
+
+--
+-- Ketidakleluasaan untuk tabel `tugas`
+--
+ALTER TABLE `tugas`
+  ADD CONSTRAINT `tugas_kd_matkul_foreign` FOREIGN KEY (`kd_matkul`) REFERENCES `matkuls` (`kd_matkul`);
+
+--
+-- Ketidakleluasaan untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`username`) REFERENCES `dosens` (`nip`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`username`) REFERENCES `mahasiswas` (`npm`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `verify_tugas`
+--
+ALTER TABLE `verify_tugas`
+  ADD CONSTRAINT `verify_tugas_kd_matkul_foreign` FOREIGN KEY (`kd_matkul`) REFERENCES `matkuls` (`kd_matkul`),
+  ADD CONSTRAINT `verify_tugas_npm_foreign` FOREIGN KEY (`npm`) REFERENCES `mahasiswas` (`npm`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
